9ce53cafbc7c2352dcbcf12c6bebacdb05f4d1c5
Patrizio Acquadro <126242700+PatrizioAcquadro@users.noreply.github.com>
2026-01-04 12:46:34 -0500

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script installs all required packages from requirements.txt for Colab.
-
-echo "Installing all required packages..."
-pip install -q -r requirements.txt
-echo "Installation complete. You may need to restart the runtime."
